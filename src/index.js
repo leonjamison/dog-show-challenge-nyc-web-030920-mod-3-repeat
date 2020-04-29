f40a3fae8edc98e459116b9ef4d5b7713d90cbf6
Leon Jamison <leonjamison@icloud.com>
2020-04-29 11:45:01 -0400

    ughhh lol

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const endPoint = 'http://localhost:3000/dogs'
 const tableBody = document.getElementsByTagName('tbody')
 // console.log(tableBody)
-const form = document.getElementsByClassName('padding margin border-round border-grey')
+const form = document.getElementsByTagName('form')
 console.log(form)
 
 
@@ -15,10 +15,11 @@ renderDogs()
 document.addEventListener('click', (e) => {
     if(e.target.textContent === 'Edit' ){ 
         let dogInfo = form[0]
-        console.log(e.target)
-        // dogInfo.name = e.target.name.value
-        // dogInfo.breed.value = e.target.breed.value
-        // dogInfo.sex.value = e.target.sex.value 
+        let target = e.target.closest('tr')
+        console.log(target)
+        dogInfo.name.value = target.name
+        dogInfo.breed.value = target.breed
+        dogInfo.sex.value = target.sex
     }
 
 
@@ -38,7 +39,7 @@ function renderDogs(){
         let tableRow = document.createElement('tr')
         tableRow.dataset.id = `${dog.id}`
         tableRow.innerHTML = 
-     `<tr><td>${dog.name} </td> <td>${dog.breed}</td> <td>${dog.sex}</td> <td><button>Edit</button></td></tr>`
+     `<tr><td value = 'name'>${dog.name} </td> <td>${dog.breed}</td> <td>${dog.sex}</td> <td><button>Edit</button></td></tr>`
         
      tableBody[0].appendChild(tableRow)
         
